c365f79241a4131c8ac0af5b13be8a82202c4747
Marek Macznik <marek.macznik@gmail.com>
2025-04-11 01:21:02 +0200

    Add: stub for implementation

---
+++ lua/base.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.version = function()
+end
+
+M.install = function()
+end
+
+M.build = function()
+end
+
+return M
+
--- lua/conan.lua
+++ lua/conan.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local cfg_file = ".nvconan"
+
 local function conan_check_or_install()
   if vim.fn.executable("conan") == 1 then
     return
@@ -28,6 +30,60 @@ local function conan_check_or_install()
 
   vim.notify("❌ Failed to install 'conan' using pip", vim.log.levels.ERROR)
 end
+
+---@class NVConanSubCommand
+---@field impl fun(args:string[], opts: table)
+---@field complete? fun(subcmd_arg_lead: string): string[]
+
+---@type table<string, NVConanSubCommand>
+local subcommand_tbl = {
+  version = {
+    impl = require("base").version
+  },
+  install = {
+    impl = require("base").install
+  },
+  build = {
+    impl = require("base").build
+  }
+}
+
+---@param opts table :h lua-guide-commands-create
+local function NVConanCmd(opts)
+    local fargs = opts.fargs
+    local subcommand_key = fargs[1]
+    local args = #fargs > 1 and vim.list_slice(fargs, 2, #fargs) or {}
+    local subcommand = subcommand_tbl[subcommand_key]
+    if not subcommand then
+        vim.notify("Rocks: Unknown command: " .. subcommand_key, vim.log.levels.ERROR)
+        return
+    end
+    subcommand.impl(args, opts)
+end
+
+vim.api.nvim_create_user_command("NVConan", NVConanCmd, {
+    nargs = "+",
+    desc = "Conan commands completions",
+    complete = function(arg_lead, cmdline, _)
+        local subcmd_key, subcmd_arg_lead = cmdline:match("^['<,'>]*NVConan[!]*%s(%S+)%s(.*)$")
+        if subcmd_key
+            and subcmd_arg_lead
+            and subcommand_tbl[subcmd_key]
+            and subcommand_tbl[subcmd_key].complete
+        then
+            return subcommand_tbl[subcmd_key].complete(subcmd_arg_lead)
+        end
+        if cmdline:match("^['<,'>]*NVConan[!]*%s+%w*$") then
+            local subcommand_keys = vim.tbl_keys(subcommand_tbl)
+            return vim.iter(subcommand_keys)
+                :filter(function(key)
+                    return key:find(arg_lead) ~= nil
+                end)
+                :totable()
+        end
+    end,
+    bang = true,
+})
 
 ---Setup the Conan plugin
 M.setup = function()
